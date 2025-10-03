69068d30d0e9b3aed313f6c60aaabd56f8fa2c1c
Jeroen Verhoest <jeroen_verhoest@hotmail.com>
2025-10-03 19:09:27 +0200

    wip

--- src/ios/ZebraScannerDevice.m
+++ src/ios/ZebraScannerDevice.m
@@ -4,19 +4,19 @@
 //
 //  Created by Zebra Scanner Device implementation
 //
-
 #import <ZebraRfidSdkFramework/ZebraRfidSdkFramework.h>
 #import <ZebraScannerFramework/ZebraScannerFramework.h>
 #import "ZebraScannerDevice.h"
 #import <Cordova/CDV.h>
 
 @implementation ZebraScannerDevice
-
 - (instancetype)init {
     self = [super init];
     if (self) {
         self.api = [srfidSdkFactory createRfidSdkApiInstance];
         self.availableRFIDReaderList = [NSMutableArray array];
+        
+        
         
         [self.api srfidSetOperationalMode:SRFID_OPMODE_ALL];
         [self.api srfidSubsribeForEvents: (SRFID_EVENT_READER_APPEARANCE |
@@ -29,7 +29,6 @@
     }
     return self;
 }
-
 - (void)getDeviceList:(CDVInvokedUrlCommand*)command commandDelegate:(NSObject<CDVCommandDelegate>*)delegate {
     CDVPluginResult* pluginResult = nil;
     if (command != nil) {
@@ -43,20 +42,14 @@
         [self.availableRFIDReaderList addObjectsFromArray:availableReaders];
         [self.availableRFIDReaderList addObjectsFromArray:activeReaders];
         
-        for (srfidReaderInfo *reader in self.availableRFIDReaderList) {
-            NSLog(@"Reader Name: %@", [reader getReaderName]);
-            NSLog(@"Reader ID: %d", [reader getReaderID]);
-        }
-        
         NSMutableArray *dataArray = [[NSMutableArray alloc] init];
         NSMutableDictionary *readerObj = nil;
         for (srfidReaderInfo *reader in self.availableRFIDReaderList) {
             readerObj = [[NSMutableDictionary alloc] init];
-            [readerObj setObject:obj.name forKey:@"name"];
-            [readerObj setObject:obj.serialNumber forKey:@"deviceID"];
+            [readerObj setObject:[reader getReaderName] forKey:@"name"];
+            //[readerObj setObject:[reader getReaderID] forKey:@"deviceID"];
             [dataArray addObject:readerObj];
         }
-
         NSError *error = nil;
         NSString *status = @"true";
         NSString *errorMsg = @"";
@@ -79,6 +72,36 @@
     }
     [delegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+- (void)connect:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate {}
+- (void)disconnect:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate {}
+- (void)getDeviceInfo:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate {}
+- (void)isConnected:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)scanRFIDs:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)search:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)setOutputPower:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)startSearch:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)stopSearch:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)subscribeScanner:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+- (void)unsubscribeScanner:(CDVInvokedUrlCommand *)command commandDelegate:(NSObject<CDVCommandDelegate> *)delegate { }
+-(void)srfidEventReaderAppeared:(srfidReaderInfo*)availableReader {
+    /* print the information about RFID reader represented by srfidReaderInfo
+     object */
+    NSLog(@"RFID reader has appeared: ID = %d name = %@\n", [availableReader getReaderID],
+          [availableReader getReaderName]);
+}
+-(void)srfidEventReaderDisappeared:(int)readerID {
+    NSLog(@"RFID reader has disappeared: ID = %d\n", readerID);
+}
+- (void)srfidEventBatteryNotity:(int)readerID aBatteryEvent:(srfidBatteryEvent *)batteryEvent { }
+- (void)srfidEventCommunicationSessionEstablished:(srfidReaderInfo *)activeReader { }
+- (void)srfidEventCommunicationSessionTerminated:(int)readerID { }
+- (void)srfidEventConnectedInterfaceNotity:(int)readerID aConnectedInterfaceEvent:(sfidConnectedInterfaceEvent *)connectedInterfaceEvent { }
+- (void)srfidEventIOTSatusNotity:(int)readerID aIOTStatusEvent:(srfidIOTStatusEvent *)iotStatusEvent { }
+- (void)srfidEventMultiProximityNotify:(int)readerID aTagData:(srfidTagData *)tagData { }
+- (void)srfidEventProximityNotify:(int)readerID aProximityPercent:(int)proximityPercent { }
+- (void)srfidEventReadNotify:(int)readerID aTagData:(srfidTagData *)tagData { }
+- (void)srfidEventStatusNotify:(int)readerID aEvent:(SRFID_EVENT_STATUS)event aNotification:(id)notificationData { }
+- (void)srfidEventTriggerNotify:(int)readerID aTriggerEvent:(SRFID_TRIGGEREVENT)triggerEvent { }
+- (void)srfidEventWifiScan:(int)readerID wlanSCanObject:(srfidWlanScanList *)wlanScanObject { }
+@end
 
-
-@end 
